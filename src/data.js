19f678e7881957a728157efedcaea0c74a2683e4
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-28 20:19:24 +0100

    try catch block inside data

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 import { renderSearchResults } from "./views/resultView.js"
+import { renderError } from "./views/errorView.js"
 
 export async function getFilmsByName(query){
     const url = `https://api.themoviedb.org/3/search/movie?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&query=${query}&page=1&include_adult=false`
+    try {
     const res = await fetch(url)
     if(!res.ok){
-        const error = `${res.status} : ${res.statusText}`
-        renderError(error)
+        throw new Error('HTTP error')
     }
     const jsonData = await res.json();
-    try {
         renderSearchResults(jsonData)  
     } catch (error) {
         renderError(error)
@@ -19,31 +19,41 @@ export async function getFilmsByName(query){
 export async function getFilmsWithFilters(year, genre, vote, keyword){
    
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_average.gte=${vote}&with_genres=${genre}&with_keywords=${keyword}&with_watch_monetization_types=flatrate`
-    const res = await fetch(url)
-    if(!res.ok){
-        const error = `${res.status} : ${res.statusText}`
-        renderError(error)
+    try{const res = await fetch(url)
+        if(!res.ok){
+            throw new Error('HTTP error')
+        }
+        const jsonData = await res.json();
+        return jsonData
+    } catch (error) {
+        renderError
     }
-    const jsonData = await res.json();
-    return jsonData
+    
 }
 
 export async function getGenres(){
-    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US")
-    if(!res.ok){
-        const error = `${res.status} : ${res.statusText}`
+    try{
+        const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US")
+        if(!res.ok){
+            throw new Error('HTTP error')
+        }
+        const jsonData = await res.json();
+        return jsonData
+    } catch (error){
         renderError(error)
     }
-    const jsonData = await res.json();
-    return jsonData
 }
 
 export async function getTopMovies(page){
-    const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&page=${page}`)
+    try {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&page=${page}`)
     if(!res.ok){
-        const error = `${res.status} : ${res.statusText}`
-        renderError(error)
-    }
+        throw new Error('HTTP error')
+    } 
     const jsonData = await res.json();
     return jsonData
+    } catch (error) {
+        renderError(error)
+    }
+
 }
