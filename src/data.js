56f6259267ed335c5c4b5796f500e42838ce74f1
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-13 09:32:21 +0200

    changes api key

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import { renderSearchResults } from "./views/resultView.js";
 import { renderError } from "./views/errorView.js";
 
 export async function getFilmsByName(query) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&query=${query}&page=1&include_adult=false`;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=48dff5b67e789d262126f79823385995&language=en-US&query=${query}&page=1&include_adult=false`;
   try {
     const res = await fetch(url);
     if (!res.ok) {
@@ -16,7 +16,7 @@ export async function getFilmsByName(query) {
 }
 
 export async function getFilmsWithFilters(year, genre, vote, keyword) {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_average.gte=${vote}&with_genres=${genre}&with_keywords=${keyword}&with_watch_monetization_types=flatrate`;
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=48dff5b67e789d262126f79823385995&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${year}&vote_average.gte=${vote}&with_genres=${genre}&with_keywords=${keyword}&with_watch_monetization_types=flatrate`;
   try {
     const res = await fetch(url);
     if (!res.ok) {
@@ -32,7 +32,7 @@ export async function getFilmsWithFilters(year, genre, vote, keyword) {
 export async function getGenres() {
   try {
     const res = await fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US"
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=48dff5b67e789d262126f79823385995&language=en-US"
     );
     if (!res.ok) {
       throw new Error("HTTP error");
@@ -47,7 +47,7 @@ export async function getGenres() {
 export async function getTopMovies(page) {
   try {
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=464e0732a4ff7c3b5e09de7baa51e9f2&language=en-US&page=${page}`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=48dff5b67e789d262126f79823385995&language=en-US&page=${page}`
     );
     if (!res.ok) {
       throw new Error("HTTP error");
